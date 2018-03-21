4fd99afd309e963be5aaaba5d6799b32edd549d7
yvan <57882634@qq.com>
2018-03-21 12:45:49 +0800

    add demmo

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
 import { sayHello } from "./greet";
 
-console.log(sayHello("TypeScript"));
+function showHello(divName: string, name: string) {
+    const el = document.getElementById(divName);
+    el.innerText = sayHello(name);
+}
+
+showHello("greeting", "TypeScript");
