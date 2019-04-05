ea2567ab808b1b187cb85afab0dd397586522046
yvan <57882634@qq.com>
2019-04-05 18:47:46 +0800

    windows last commit

--- examples/type-advance/type-assert-protection.ts
+++ examples/type-advance/type-assert-protection.ts
@@ -12,11 +12,12 @@ function fixed(name: string | null): string {
   function postfix(epithet: string) {
     return name!.charAt(0) + '.  the ' + epithet; // ok
   }
-  // name = name || "Bob";
+  name = name || "Bob";
   return postfix("great");
 }
 /*
 本例使用了嵌套函数，因为编译器无法去除嵌套函数的null（除非是立即调用的函数表达式）。 因为它无法跟踪所有对嵌套函数的调用，尤其是你将内层函数做为外层函数的返回值。 如果无法知道函数在哪里被调用，就无法知道调用时 name的类型。
+tsc "d:\workspace\web\typescript\typescript2-demo\examples\type-advance\type-assert-protection.ts" --strictNullChecks
 */
 console.log(broken(null));
 console.log(fixed(null));
