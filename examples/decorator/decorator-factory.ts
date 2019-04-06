05e1e3fa5cdcc9b96a397578f8081e3376292de0
yvan <57882634@qq.com>
2019-04-06 22:39:02 +0800

    decorator

--- examples/decorator/decorator-factory.ts
+++ examples/decorator/decorator-factory.ts
@@ -1,21 +1,10 @@
-function f() {
-  console.log("f(): evaluated");
-  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-      console.log("f(): called");
+/*
+如果我们要定制一个修饰器如何应用到一个声明上，我们得写一个装饰器工厂函数。
+装饰器工厂就是一个简单的函数，它返回一个表达式，以供装饰器在运行时调用
+*/
+
+function color(value: string) { // 这是一个装饰器工厂
+  return function (target) { //  这是装饰器
+      // do something with "target" and "value"...
   }
 }
-
-function g() {
-  console.log("g(): evaluated");
-  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-      console.log("g(): called");
-  }
-}
-
-class C {
-  @f()
-  @g()
-  method() {}
-}
-
-new C().method();
