3af550332aec10fb60397bf5771385eb1ec0d314
yvan <57882634@qq.com>
2019-04-08 09:57:30 +0800

    types-check

--- examples/decorator/class-decorator.ts
+++ examples/decorator/class-decorator.ts
@@ -1,3 +1,8 @@
+/*
+类装饰器在类声明之前被声明（紧靠着类声明）。 类装饰器应用于类构造函数，可以用来监视，修改或替换类定义。
+类装饰器不能用在声明文件中( .d.ts)，也不能用在任何外部上下文中（比如declare的类）。
+*/
+
 function classDecorator<T extends {new(...args:any[]):{}}>(constructor:T) {
   return class extends constructor {
       newProperty = "new property";
--- examples/decorator/reflect-metadata-demo.ts
+++ examples/decorator/reflect-metadata-demo.ts
@@ -1,4 +1,3 @@
-
 import "reflect-metadata";
 
 class Point {
@@ -29,3 +28,22 @@ function validate<T>(target: any, propertyKey: string, descriptor: TypedProperty
         set(value);
     }
 }
+
+/*
+// TypeScript编译器可以通过@Reflect.metadata装饰器注入设计阶段的类型信息。 你可以认为它相当于下面的TypeScript：
+
+class Line {
+    private _p0: Point;
+    private _p1: Point;
+
+    @validate
+    @Reflect.metadata("design:type", Point)
+    set p0(value: Point) { this._p0 = value; }
+    get p0() { return this._p0; }
+
+    @validate
+    @Reflect.metadata("design:type", Point)
+    set p1(value: Point) { this._p1 = value; }
+    get p1() { return this._p1; }
+}
+*/
